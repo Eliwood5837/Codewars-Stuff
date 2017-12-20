d44e61634c810501c11f4c28918c2d3d83277d92
Eugene <eugene1998@sbcglobal.net>
2017-12-20 16:40:02 -0600

    Find if string ends with w/e

---
+++ StringEndsWith.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Eugene on 12/20/2017.
+ */
+public class StringEndsWith {
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+    
+}
